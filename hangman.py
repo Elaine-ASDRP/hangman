86e7f0ce3a0dd517d6ab9e3f9feb02c83ed815e0
AnanyaBaphna <ananya.baphna@students.asdrp.org>
2026-07-02 17:32:08 -0700

    Fix formatting and add alphabet list in hangman.py

--- hangman.py
+++ hangman.py
@@ -1,4 +1,4 @@
 # This is the Hangman python script
 from engilsh_words import get_inglish_words_set
-
+alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
 print "Let's play hangman!"
